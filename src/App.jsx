1eeed4a5b3e606269054abcb6f906b1faf4c26b9
Keven-Duarte <kevenoliveiraduarte2020@gmail.com>
2026-03-10 07:34:03 -0300

    Primeira versão

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Topo from './components/Topo'
+
+function App() {
+  return (
+    <div>
+      <header>
+        <Topo/>
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Topo/index.jsx
@@ -0,0 +1,26 @@
+import './Topo.css'
+
+export default function Topo() {
+    return (
+        <header>
+            <div className='anuncio'>
+                <p>Tudo em 10X sem juros!</p>
+            </div>
+
+            <div className='topo'>
+                <picture>
+                    <a href="">
+                        <img src="./logoLoja.png" alt="" />
+                    </a>
+                </picture>
+
+                <nav>
+                    <a href="#">Nome</a>
+                    <a href="#">Produtos</a>
+                    <a href="#">Lançamentos</a>
+                    <a href="#">Contato</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
